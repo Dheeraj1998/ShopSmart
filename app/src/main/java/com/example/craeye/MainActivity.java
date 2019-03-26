fde0bef4788515b84ee2117d78b7fc7991c85183
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2019-03-26 18:31:27 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/craeye/MainActivity.java
+++ app/src/main/java/com/example/craeye/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.craeye;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.LoginFilter;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
@@ -15,9 +23,37 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase database;
     private DatabaseReference doRef;
+
+    private BluetoothAdapter bluetoothAdapter;
+    private ArrayList bluetoothDevices = new ArrayList<String>();
+
+    int total_count = 0;
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                bluetoothDevices.add("" + device.getName());
+
+                try {
+                    if (device.getName().equals("BEACON_01")) {
+                        showAd();
+                    }
+                }
+
+                catch (Exception error) {
+
+                }
+                Log.i("custom", bluetoothDevices.toString());
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +67,25 @@ public class MainActivity extends AppCompatActivity {
         Log.i("custom", sharedpreferences.getString("dob", ""));
         Log.i("custom", sharedpreferences.getString("gender", ""));
 
-        
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        final IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+
+        startBluetooth();
         updateDailyOffer();
+
+        final Handler handler = new Handler();
+        final int delay = 5000;
+
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                searchBeacon();
+                startBluetooth();
+                handler.postDelayed(this, 5000);
+            }
+        }, delay);
     }
 
     void updateDailyOffer(){
@@ -67,5 +120,81 @@ public class MainActivity extends AppCompatActivity {
                 Log.w("custom", "Failed to read value.", error.toException());
             }
         });
+    }
+
+    void startBluetooth(){
+        bluetoothAdapter.startDiscovery();
+
+        if(bluetoothAdapter.isDiscovering()){
+            bluetoothAdapter.cancelDiscovery();
+        }
+    }
+
+    void searchBeacon() {
+        if(!bluetoothDevices.contains("BEACON_01")){
+            TextView beacon_name = findViewById(R.id.info_text_3);
+            beacon_name.setText("No available offer.");
+        }
+
+        bluetoothDevices.clear();
+    }
+
+    void showAd() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("beacon_locations");
+        final String[] rack_number = new String[3];
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                rack_number[0] = dataSnapshot.child("BEACON_1").getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        myRef = database.getReference("camera_01");
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                rack_number[1] = dataSnapshot.child("age").getValue().toString();
+                rack_number[2] = dataSnapshot.child("gender").getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        myRef = database.getReference("rack").child("02").child("Electronics");
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                TextView beacon_name = findViewById(R.id.info_text_3);
+
+                try {
+                    beacon_name.setText("BEACON_1 \n" + dataSnapshot.child(rack_number[2].substring(2, rack_number[2].length() - 2)).child(rack_number[1].substring(2, rack_number[1].length() - 2)).getValue().toString());
+                }
+
+                catch (Exception error) {
+                    beacon_name.setText("BEACON_1");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
     }
 }
